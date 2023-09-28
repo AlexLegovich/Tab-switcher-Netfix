537f67778e1d88f26cf5d667bf639ffb8edc1e90
Your Name <gussev.sanya@hotmail.com>
2023-09-28 22:25:26 +0300

    first commit

---
+++ js/app.js
@@ -0,0 +1,48 @@
+(() => {
+    "use strict";
+    function isWebp() {
+        function testWebP(callback) {
+            let webP = new Image;
+            webP.onload = webP.onerror = function() {
+                callback(webP.height == 2);
+            };
+            webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
+        }
+        testWebP((function(support) {
+            let className = support === true ? "webp" : "no-webp";
+            document.documentElement.classList.add(className);
+        }));
+    }
+    let addWindowScrollEvent = false;
+    setTimeout((() => {
+        if (addWindowScrollEvent) {
+            let windowScroll = new Event("windowScroll");
+            window.addEventListener("scroll", (function(e) {
+                document.dispatchEvent(windowScroll);
+            }));
+        }
+    }), 0);
+    const tabsBtn = document.querySelectorAll(".tabs__nav-btn");
+    const tabsItems = document.querySelectorAll(".tabs__item");
+    tabsBtn.forEach(onTabClick);
+    function onTabClick(item) {
+        item.addEventListener("click", (function() {
+            let currentBtn = item;
+            let tabId = currentBtn.getAttribute("data-tab");
+            let currentTab = document.querySelector(tabId);
+            if (!currentBtn.classList.contains("active")) {
+                tabsBtn.forEach((function(item) {
+                    item.classList.remove("active");
+                }));
+                tabsItems.forEach((function(item) {
+                    item.classList.remove("active");
+                }));
+                currentBtn.classList.add("active");
+                currentTab.classList.add("active");
+            }
+        }));
+    }
+    document.querySelector(".tabs__nav-btn").click();
+    window["FLS"] = true;
+    isWebp();
+})();
